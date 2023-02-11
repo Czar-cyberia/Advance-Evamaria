63918d4baa352a6eae080a095ef1f40a3a27610a
Czar-cyberia <86222643+Czar-cyberia@users.noreply.github.com>
2023-02-11 11:42:28 +0530

    forward index solving

--- plugins/index.py
+++ plugins/index.py
@@ -49,7 +49,7 @@ async def index_files(bot, query):
     await index_files_to_db(int(lst_msg_id), chat, msg, bot)
 
 
-@Client.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
+@Client.on_message(filters.forwarded & filters.private & filters.incoming)
 async def send_for_index(bot, message):
     if message.forward_from_chat.type != 'channel':
         return
